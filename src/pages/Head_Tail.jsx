aa7259f342c0888c5f671aaa4a31d30f8a9026b9
raj <rajtech645@gmail.com>
2025-07-09 12:16:24 +0530

    writing logic

--- src/pages/Head_Tail.jsx
+++ src/pages/Head_Tail.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
-
+import { Toaster } from 'react-hot-toast'
+import { toast } from "react-hot-toast"
 const Head_Tail = () => {
 
 
@@ -8,18 +9,44 @@ const Head_Tail = () => {
 
 
   const submit = () => {
+    if (!choose) {
+      toast.error("select something!")
+      return
+    }
+    console.log("choosed ==>", choose)
+
+    let clonedArray = [...cols];
+    console.log("cols array==>", cols)
+    console.log("cloned array ==>", clonedArray)
+
+
+
+    let index = clonedArray.findIndex((el) => el.type === choose);
+    console.log("index ==>", index)
+
+    if (index === -1) {
+      //{ [type:"H",values:["H"]] } : 
+      clonedArray.push({ type: choose, values: [choose] });
+    } else {
+      clonedArray[index].values.push(choose);
+    }
+
+    setCols(clonedArray);
+  
 
   }
 
   return (
     <div>
+      <Toaster position='top-center' reverseOrder={false} />
+
 
       <div>
         <p>Select value :</p>
-        {choose && <p>Selected value is : {choose}</p>}
+        {choose && <p>Your Selected value is : {choose}</p>}
 
-        <select style={{cursor:"pointer",padding:3}} onChange={(e) => setChoose(e.target.value)}>
-          <option>select T or H</option>
+        <select style={{ cursor: "pointer", padding: 3 }} onChange={(e) => setChoose(e.target.value)}>
+          <option value={""}>select T or H</option>
           <option value={"H"}> H</option>
           <option value={"T"}> T</option>
 
@@ -30,9 +57,47 @@ const Head_Tail = () => {
       </div>
 
 
-      <div style={{marginTop:5}}>
+      <div style={{ marginTop: 5 }}>
         <button onClick={() => submit()}>Submit</button>
       </div>
+      {/* display */}
+
+      <div>
+        <p>Display :</p>
+
+        <div style={{ background: "grey" }}>
+          <div style={{ display: "flex", gap: "20px", marginTop: "20px" }}>
+            {cols.map((col, index) => (
+              <div key={index}>
+                {col.values.map((val, i) => (
+                  <div key={i}>{val}</div>
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
+
+
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     </div>
   )
 }
