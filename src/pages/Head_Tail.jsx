93f184ceb5a1c6c82b8d3fa16438da76c1c94792
raj <rajtech645@gmail.com>
2025-07-09 11:43:10 +0530

    select and button created

--- src/pages/Head_Tail.jsx
+++ src/pages/Head_Tail.jsx
@@ -3,20 +3,35 @@ import React, { useState, useEffect } from 'react'
 const Head_Tail = () => {
 
 
-  const [choose,setChoose]=useState("")
-  const [cols,setCols]=useState([])
+  const [choose, setChoose] = useState("")
+  const [cols, setCols] = useState([])
 
 
-  const submit=()=>{
+  const submit = () => {
 
   }
 
   return (
     <div>
 
-
       <div>
-        <button onClick={()=>submit()}>Submit</button>
+        <p>Select value :</p>
+        {choose && <p>Selected value is : {choose}</p>}
+
+        <select style={{cursor:"pointer",padding:3}} onChange={(e) => setChoose(e.target.value)}>
+          <option>select T or H</option>
+          <option value={"H"}> H</option>
+          <option value={"T"}> T</option>
+
+
+
+        </select>
+
+      </div>
+
+
+      <div style={{marginTop:5}}>
+        <button onClick={() => submit()}>Submit</button>
       </div>
     </div>
   )
